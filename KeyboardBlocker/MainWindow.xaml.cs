6a2fcadd5cc244e03916eb9b2c8487212d31768c
leogalencar <leonardogalencar444@gmail.com>
2022-12-02 19:13:18 -0300

    Adicionar arquivos de projeto.

---
+++ KeyboardBlocker/MainWindow.xaml.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace KeyboardBlocker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public const int WH_KEYBOARD_LL = 13;
+        public const int WM_KEYDOWN = 0x0100;
+        public const int WM_KEYUP = 0x0101;
+        public const int WM_SYSKEYDOWN = 0x0104;
+        public const int WM_SYSKEYUP = 0x0105;
+        public const int VK_TAB = 0x9;
+        public const int VK_MENU = 0x12; /* Alt key */
+        public const int VK_ESCAPE = 0x1B;
+        public const int VK_F4 = 0x73;
+        public const int VK_LWIN = 0x5B;
+        public const int VK_RWIN = 0x5C;
+        public const int VK_CONTROL = 0x11;
+        public const int VK_LCONTROL = 0xA2;
+        public const int VK_RCONTROL = 0xA3;
+        public bool buttonControl;
+        public string buttonStatus = "Ativar";
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public class KeyBoardHookStruct
+        {
+            public int vkCode;
+            public int scanCode;
+            public int flags;
+            public int time;
+            public int dwExtraInfo;
+        }
+
+        [DllImport("user32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hInstance, int threadId);
+
+        [DllImport("user32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern bool UnhookWindowsHookEx(int idHook);
+
+        [DllImport("user32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int CallNextHookEx(int idHook, int nCode, Int32 wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        public delegate int HookProc(int nCode, Int32 wParam, IntPtr lParam);
+        static int hKeyboardHook = 0;
+        HookProc KeyboardHookProcedure;
+
+        public void HookStart()
+        {
+            if (hKeyboardHook == 0)
+            {
+                KeyboardHookProcedure = new HookProc(KeyboardHookProc);
+                using (Process curProcess = Process.GetCurrentProcess())
+                using (ProcessModule curModule = curProcess.MainModule)
+                {
+                    IntPtr hModule = GetModuleHandle(curModule.ModuleName);
+                    hKeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardHookProcedure, hModule, 0);
+                }
+                if (hKeyboardHook == 0)
+                {
+                    int error = Marshal.GetLastWin32Error();
+                    HookStop();
+                    throw new Exception("SetWindowsHookEx() function failed. " + "Error code: " + error.ToString());
+                }
+            }
+        }
+
+        public void HookStop()
+        {
+            bool retKeyboard = true;
+            if (hKeyboardHook != 0)
+            {
+                retKeyboard = UnhookWindowsHookEx(hKeyboardHook);
+                hKeyboardHook = 0;
+            }
+            if (!(retKeyboard))
+            {
+                throw new Exception("UnhookWindowsHookEx failed.");
+            }
+        }
+
+        private int KeyboardHookProc(int nCode, int wParam, IntPtr lParam)
+        {
+            KeyBoardHookStruct kbh = (KeyBoardHookStruct)Marshal.PtrToStructure(lParam, typeof(KeyBoardHookStruct));
+            bool bMaskKeysFlag = false;
+            switch (wParam)
+            {
+                case WM_KEYDOWN:
+                case WM_KEYUP:
+                case WM_SYSKEYDOWN:
+                case WM_SYSKEYUP:
+                    bMaskKeysFlag = ((kbh.vkCode == VK_TAB) && (kbh.flags == 32))      /* Tab + Alt */
+                                    | ((kbh.vkCode == VK_ESCAPE) && (kbh.flags == 32))   /* Esc + Alt */
+                                    | ((kbh.vkCode == VK_F4) && (kbh.flags == 32))       /* F4 + Alt */
+                                    | ((kbh.vkCode == VK_LWIN) && (kbh.flags == 1))    /* Left Win */
+                                    | ((kbh.vkCode == VK_RWIN) && (kbh.flags == 1))    /* Right Win */
+                                    | ((kbh.vkCode == VK_ESCAPE) && (kbh.flags == 0)) /* Ctrl + Esc */
+                                    | (kbh.vkCode != 00);   /* All keys */
+                    break;
+                default:
+                    break;
+            }
+
+            if (bMaskKeysFlag == true)
+            {
+                return 1;
+            }
+            else
+            {
+                return CallNextHookEx(hKeyboardHook, nCode, wParam, lParam);
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+
+            if (buttonStatus == "Ativar")
+            {
+                buttonControl = true;
+                buttonStatus = "Desativar";
+                textStatus.Text = "Enabled";
+                startButtonImageBrush.ImageSource = new BitmapImage(new Uri("C:/Users/fnat1/Documents/Projetos Programação/C#/KeyboardBlocker/KeyboardBlocker/img/power_button_on.png", UriKind.RelativeOrAbsolute));
+            }
+            else if (buttonStatus == "Desativar")
+            {
+                buttonControl = false;
+                buttonStatus = "Ativar";
+                textStatus.Text = "Disabled";
+                startButtonImageBrush.ImageSource = new BitmapImage(new Uri("C:/Users/fnat1/Documents/Projetos Programação/C#/KeyboardBlocker/KeyboardBlocker/img/power_button_off.png", UriKind.RelativeOrAbsolute));
+            }
+            else
+            {
+
+            }
+
+            if (buttonControl == true)
+            {
+                HookStart();
+            }
+            else
+            {
+                HookStop();
+            }
+        }
+
+        private void closeApp(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void minimizeApp(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.WindowState = WindowState.Minimized;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Drag(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch (Exception)
+            {
+
+                //throw;
+            }
+        }
+    }
+}
